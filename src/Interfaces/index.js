66143b5671f68a9dc6152c70a0464f7c6d172f4b
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2023-08-29 17:48:47 +0300

    Restart For The Second Time :)

--- src/Interfaces/index.js
+++
@@ -1,4 +0,0 @@
-import AddInterface from "./AddInterface";
-import ViewInterface from "./ViewInterface";
-
-export { AddInterface, ViewInterface };
--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,5 @@
-import Button from "./Button/Button";
-import ButtonList from "./ButtonList/ButtonList";
-import ButtonWithIcon from "./ButtonWithIcon/ButtonWithIcon";
-import DataChunck from "./DataChunck/DataChunck";
-import DataHandler from "./DataHandler/DataHandler";
-import HeaderList from "./HeaderList/HeaderList";
 import Logo from "./Logo/Logo";
-import MultipletButton from "./MultipletButton/MultipletButton";
-import MultipletInput from "./MultipletInput/MultipletInput";
 import PasswordInput from "./PasswordInput/PasswordInput";
-import TableTile from "./TableTile/TableTile";
 import TextInput from "./TextInput/TextInput";
-import Title from "./Title/Title";
 
-import Multiple from "./Multiple/Multiple";
-import ListOfButtons from "./ListOfButtons/ListOfButtons";
-import Navigation from "./Navigation/Navigation";
-import InputWithLabel from "./InputWithLabel/InputWithLabel";
-
-export { Button, ButtonList, ButtonWithIcon, DataChunck, DataHandler, HeaderList, Logo, MultipletButton, MultipletInput, PasswordInput, TableTile, TextInput, Title, Multiple, ListOfButtons, Navigation, InputWithLabel };
+export { Logo, PasswordInput, TextInput };
