1d1a9a24fdf85c8cd766e052282975519d28b049
Hamza saad aldeen <hamzasaadaldeen2002@gmail.com>
2023-09-04 16:55:49 +0300

    working grades page and moidified app.css to make it simple (check it)

--- src/Pages/ShowGrades.js
+++ src/Pages/ShowGrades.js
@@ -50,6 +50,15 @@ useEffect(() => {
       {/* add Grade button */}
       <div className="text-purple ms-auto add-grade-btn rounded fw-bold pointer">+ Add new Grade</div>
       {/* drop down classes from grade */}
+      <div className="ms-auto mt-3 row">
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle" type="button"
+            id = "class-dropdown" data-bs-toggle="dropdown"
+          >
+            Grade 1
+          </button>
+        </div>
+      </div>
     </div>
     </>
   );
