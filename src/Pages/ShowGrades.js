ef61f001d123f762377c3af6e58b93eb36039efd
Hamza saad aldeen <hamzasaadaldeen2002@gmail.com>
2023-09-06 13:38:59 +0300

    still have the popup and fixed the whole page

--- src/Pages/ShowGrades.js
+++ src/Pages/ShowGrades.js
@@ -11,6 +11,7 @@ function ShowGrades() {
   }
 
   const [grade, setGrade] = useState([]);
+  const [activeEditGrade, setActiveEditGrade] = useState({});
 
   useEffect(() => {
     fetch(`${handlers.host}/general/getAllGradesWithClassesAndSubjects`, {
@@ -53,12 +54,6 @@ function ShowGrades() {
   return (
     <>
       <div className="container w-100 pt-5 grade">
-        <div className="class-info">
-          <div className="d-flex justify-content-end">
-            <FontAwesomeIcon icon={faDownload} className="fs-3 text-purple"/>
-            <FontAwesomeIcon icon={faClose} className=" ms-5 fs-3 fw-bold text-purple d-flex justify-content-center align-items-center"/>
-          </div>
-        </div>
         {/* main buttons */}
         <div className="d-flex justify-content-around w-50 fw-bold ms-auto school">
           {menu.map((item) => (
@@ -92,42 +87,6 @@ function ShowGrades() {
         <div>
           <div className="ms-auto mt-5 row grade-menu gx-5 gy-5">
             {grade.map((data) => (
-              <>
-                <div
-                className="dropdown col-3 grade-number text-end"
-                key={data.id}
-              >
-                <button
-                  className="btn dropdown-toggle text-gray fw-bold border-0"
-                  type="button"
-                  id={`class-dropdown${data.id}`}
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Grade {data.id}
-                </button>
-                <ul
-                  className="dropdown-menu pointer"
-                  aria-labelledby={`class-dropdown${data.id}`}
-                  onClick={() => {
-                    console.log("clicked" + data.id);
-                  }}
-                >
-                  {data.g_classes.map((classes) => (
-                    <>
-                    <li className="text-purple class" key={classes.id}>
-                      Class {classes.name}
-                    </li>
-                    {/* fuck */}
-                    <li className="text-purple class" key={classes.id}>
-                      Class {classes.name}
-                    </li>
-                    </>
-                    
-                  ))}
-                </ul>
-                
-              </div>
               <div
                 className="dropdown col-3 grade-number text-end"
                 key={data.id}
@@ -145,25 +104,32 @@ function ShowGrades() {
                   className="dropdown-menu pointer"
                   aria-labelledby={`class-dropdown${data.id}`}
                   onClick={() => {
-                    console.log("clicked" + data.id);
+                    const active = { active: true, id: data.id };
+                    setActiveEditGrade(active);
                   }}
                 >
                   {data.g_classes.map((classes) => (
-                    <>
                     <li className="text-purple class" key={classes.id}>
                       Class {classes.name}
                     </li>
-                    {/* fuck */}
-                    <li className="text-purple class" key={classes.id}>
-                      Class {classes.name}
-                    </li>
-                    </>
-                    
                   ))}
                 </ul>
-                
+                <div className={ (activeEditGrade.id == data.id && activeEditGrade.active) ? "class-info" : "class-info d-none"}>
+                  <div className="d-flex justify-content-end">
+                    <FontAwesomeIcon
+                      icon={faDownload}
+                      className="fs-3 text-purple"
+                    />
+                    <FontAwesomeIcon
+                      icon={faClose}
+                      className=" ms-5 fs-3 fw-bold text-purple d-flex justify-content-center align-items-center"
+                    />
+                  </div>
+                  <div className="mt-3">
+                    <h2 className="text-center">Grade {data.id}</h2>
+                  </div>
+                </div>
               </div>
-              </>
             ))}
           </div>
         </div>
