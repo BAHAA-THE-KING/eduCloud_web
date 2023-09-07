e15101e702ad59862ee428b3df5661f628a5f6ea
Hamza saad aldeen <hamzasaadaldeen2002@gmail.com>
2023-09-07 13:43:51 +0300

    merging

--- src/Pages/ShowGrades.js
+++ src/Pages/ShowGrades.js
@@ -84,76 +84,69 @@ function ShowGrades() {
         <div>
           <div className="ms-auto mt-5 row grade-menu gx-5 gy-5">
             {grade.map((data) => (
-              <>
-                <div
-                  className="dropdown col-3 grade-number text-end"
-                  key={data.id}
-                >
-                  <button
-                    className="btn dropdown-toggle text-gray fw-bold border-0"
-                    type="button"
-                    id={`class-dropdown${data.id}`}
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Grade {data.id}
-                  </button>
-                  <ul
-                    className="dropdown-menu pointer"
-                    aria-labelledby={`class-dropdown${data.id}`}
-                    onClick={() => {
-                      console.log("clicked" + data.id);
-                    }}
-                  >
-                    {data.g_classes.map((classes) => (
-                      <>
-                        <li className="text-purple class" key={classes.id}>
-                          Class {classes.name}
-                        </li>
-                        {/* fuck */}
-                        <li className="text-purple class" key={classes.id}>
-                          Class {classes.name}
-                        </li>
-                      </>
+              <div
+                className="dropdown col-3 grade-number text-end"
+                key={data.id}
+              >
+                <GradeDropDownMenu id={data.id} classObject={data.g_classes} fun={setActive} />
 
-                    ))}
-                  </ul>
-
-                </div>
-                <div
-                  className="dropdown col-3 grade-number text-end"
-                  key={data.id}
+                {/*       
+                <button
+                  className="btn dropdown-toggle text-gray fw-bold border-0"
+                  type="button"
+                  id={`class-dropdown${data.id}`}
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
                 >
-                  <button
-                    className="btn dropdown-toggle text-gray fw-bold border-0"
-                    type="button"
-                    id={`class-dropdown${data.id}`}
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Grade {data.id}
-                  </button>
-                  <ul
-                    className="dropdown-menu pointer"
-                    aria-labelledby={`class-dropdown${data.id}`}
-                    onClick={() => {
-                      console.log("clicked" + data.id);
-                    }}
-                  >
-                    {data.g_classes.map((classes) => (
-                      <>
-                        <li className="text-purple class" key={classes.id}>
-                          Class {classes.name}
-                        </li>
-                        {/* fuck */}
-                        <li className="text-purple class" key={classes.id}>
-                          Class {classes.name}
-                        </li>
-                      </>
-                    ))}
-                  </ul>
+                  Grade {data.id}
+                </button>
+                <ul
+                  className="dropdown-menu pointer"
+                  aria-labelledby={`class-dropdown${data.id}`}
+                  onClick={() => {
+                    const active = { active: true, id: data.id };
+                    setActiveEditGrade(active);
+                  }}
+                >
+                  {data.g_classes.map((classes) => (
+                    <li className="text-purple class" key={classes.id}>
+                      Class {classes.name}
+                    </li>
+                  ))}
+                </ul> */}
+                <div className={(activeEditGrade.id == data.id && activeEditGrade.active) ? "layer" : "class-info d-none"}></div>
+                <div className={(activeEditGrade.id == data.id && activeEditGrade.active) ? "class-info" : "class-info d-none"}>
+                  <div className="d-flex justify-content-end">
+                    
+                    <FontAwesomeIcon
+                      icon={faDownload}
+                      className="fs-3 text-purple pointer"
+                    />
+                    <FontAwesomeIcon
+                      icon={faClose}
+                      className=" ms-5 fs-3 fw-bold text-purple pointer"
+                      onClick={() => {
+                        setActiveEditGrade({active: false, id : data.id})
+                      }}
+                    />
+                  </div>
+                  <div className="mt-3">
+                    <h2 className="text-center text-purple mb-5">Grade {data.id}</h2>
+                    <div>
+                      <div className="text-right text-gray border-bottom border-3">Main data</div>
+                      <div className="mt-3 d-flex justify-content-around align-items-center">
+                        <div><label className="text-purple me-3">Number of classes</label> <input className="text-purple" type="number" value={data.g_classes.length} /></div>
+                        <div><label className="text-purple me-3">Supervisor</label>  <input className="text-purple" type="text" value="ahmad" /></div>
+                      </div>
+                      <div className="text-right text-gray border-bottom border-3 mt-3">Subject data</div>
+                      <div className="mt-3 mb-5 d-flex justify-content-around align-items-center">
+                        <div><label className="text-purple me-3">Grade subjects</label> <input className="text-purple" type="number" value={data.subjects.length} /></div>
+                        <div><label className="text-purple me-3">Supervisor</label>  <input className="text-purple" type="text" value="ahmad" /></div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </>
+              </div>
             ))}
           </div>
         </div>
